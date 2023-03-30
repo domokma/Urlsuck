b0ebbc0906434d8a4394b11eaca3df8ce77265bb
domokma <49523361+domokma@users.noreply.github.com>
2023-03-30 18:52:34 +0800

    Update Urlsuck.py

--- Urlsuck.py
+++ Urlsuck.py
@@ -3,7 +3,6 @@ import re
 import time
 import os
 
-# 填写url
 url = input("输入要吸的url：")
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
