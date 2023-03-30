bf5b51ca2618bef0bf9ce19b9cd30a71f37f1397
domokma <49523361+domokma@users.noreply.github.com>
2023-03-30 18:20:42 +0800

    Update Urlsuck.py

--- Urlsuck.py
+++ Urlsuck.py
@@ -4,19 +4,14 @@ import time
 import os
 
 # 填写url
-url = input("请输入地址：")
-# 访问时带上user-agent
+url = input("输入要吸的url：")
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
-# 通过requests关掉警告
 requests.packages.urllib3.disable_warnings()
-# 访问url
 response = requests.get(url, headers=headers, verify=False)
-# 通过正则表达试提取页面中包括http和https的url地址和跳转地址，生成列表1
 pattern = re.compile('(https?://[^\s]+)')
 urls = pattern.findall(response.text)
-print("列表1：", urls)
-# 再次访问列表1中的url，再次重复提取页面的url，生成列表2
+print("list1：", urls)
 urls2 = []
 for url in urls:
     try:
@@ -24,16 +19,15 @@ for url in urls:
         urls2 += pattern.findall(response.text)
     except:
         pass
-print("列表2：", urls2)
-# 将列表1，列表2的结果print出来，并且输出到txt，文件名使用当前系统时间命名
+print("list2：", urls2)
 filename = time.strftime("%Y%m%d%H%M%S", time.localtime()) + ".txt"
 with open(filename, "w") as f:
-    f.write("列表1：\n")
+    f.write("list1：\n")
     for url in urls:
         f.write(url + "\n")
-    f.write("\n列表2：\n")
+    f.write("\nlist2：\n")
     for url in urls2:
         f.write(url + "\n")
-print("结果已输出到文件：", filename)
+print("suck complete：", filename)
 
 os.startfile(filename)
